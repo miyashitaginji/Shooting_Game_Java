899844d26c97d1548778eea2e91733c3bf36b4aa
miyashitaginji <zeal23310189@fsgcl.jp>
2024-06-25 10:17:17 +0900

    13章終わりました

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -4,28 +4,59 @@ public class GameFrame extends MyFrame{
    public void run() {
 	   GameWorld.player=new Player(100,300,0,0);
 	   addKeyListener(GameWorld.player);
-	   GameWorld.playerBullets=new Vector<PlayerBullet>();
-	   GameWorld.enemies=new Vector<Enemy>();
-	   GameWorld.enemies.add(new EnemyBase(100,50,1,0));
-   
+	   GameWorld.stage=1;
+	   GameWorld.score=0;
 	   while(true) {
-		   clear();
-	   GameWorld.player.draw(this);
-	   GameWorld.player.move();
-	   movePlayerBullets();
-	  moveEnemies();
-	  
-	  checkPlayerAndEnemies();
-	  checkPlayerBulletsAndEnemies();
+		   GameWorld.playerBullets=new Vector<PlayerBullet>();
+		   GameWorld.enemies=new Vector<Enemy>();
+		   GameWorld.enemies.add(new EnemyBase(100,50,GameWorld.stage,0));
+		   GameWorld.enterPressed=false;
 
-	  sleep(0.033);
-	  }
-	 } 
+		   while(true) {
+			   clear();
+			   drawString("stage="+GameWorld.stage,300,50,15);
+			   drawString("score="+GameWorld.score,300,80,15);
+			   GameWorld.player.draw(this);
+			   GameWorld.player.move();
+			   movePlayerBullets();
+			   moveEnemies();
+			   checkPlayerAndEnemies();
+			   checkPlayerBulletsAndEnemies();
+			   if(GameWorld.enemies.size()==0) {
+				   setColor(0,0,0);
+				   drawString("クリア！",100,200,40);
+				   if(GameWorld.enterPressed) {
+					   GameWorld.stage++;
+					   break;
+				   }
+			   }else if(GameWorld.player.y<0) {
+				   setColor(0,0,0);
+				   drawString("ゲームオーバー！",50,200,40);
+				   if(GameWorld.enterPressed) {
+					   GameWorld.stage=1;
+					   GameWorld.score=0;
+					   break;
+				   }
+			   }
+
+			   sleep(0.033);
+		   }
+	   } 
+   }
    public void moveEnemies() {
 	   for (int i=0 ; i<GameWorld.enemies.size(); i++) {
 		   Enemy e=GameWorld.enemies.get(i);
 		   e.draw(this);
 		   e.move();
+	   }
+	   int i=0;
+	   while (i<GameWorld.enemies.size()) {
+		   Enemy e=GameWorld.enemies.get(i);
+		   if(e.y>400) {
+			   GameWorld.enemies.remove(i);
+		   }else {
+			   i++;
+		   }
 	   }
    } 
    public void movePlayerBullets(){
@@ -63,6 +94,7 @@ public class GameFrame extends MyFrame{
 			  e.life--;
 			   }
 			   if(e.life<=0) {
+				   GameWorld.score+=e.score;
 			  GameWorld.enemies.remove(j);
 		   }else {
 			   j++;
